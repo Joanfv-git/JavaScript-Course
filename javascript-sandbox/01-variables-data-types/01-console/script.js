5f6c7a9dec1d47e420a6f325eede757b2c92745f
JoanFV <joanfernandezvilana@gmail.com>
2024-07-29 19:57:32 +0200

    Console

---
+++ javascript-sandbox/01-variables-data-types/01-console/script.js
@@ -0,0 +1,3 @@
+//To open the console in Chrome, right-click anywhere on the page, select Inspect, and then click on the Console tab or press Ctrl+Shift+J or press f12 and click on the Console tab.
+//Print a message to the console
+console.log("Hello, World!");
