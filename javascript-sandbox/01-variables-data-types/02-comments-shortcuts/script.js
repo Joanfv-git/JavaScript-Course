02d028e95f3e8106e49908cf4a65d45f080da25a
JoanFV <joanfernandezvilana@gmail.com>
2024-07-29 19:58:38 +0200

    Comments and Shortcuts

---
+++ javascript-sandbox/01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,28 @@
+//Comments and shortcuts
+
+//Single line comment
+/* Multi-line comment */
+
+//Shortcut to create a single line commennt in Visual Studio Code: Ctrl + /
+//Shortcut to create a multi-line comment in Visual Studio Code: Shift + Alt + A
+//Shortcut to comment out a block of code in Visual Studio Code: Ctrl + K + C
+//Shortcut to uncomment a block of code in Visual Studio Code: Ctrl + K + U
+
+//Shortcut to move to the beginning of the line in Visual Studio Code: Inicio
+//Shortcut to move to the end of the line in Visual Studio Code: Fin
+
+//Shortcut to move to the beginning of the line in Visual Studio Code: Ctrl + ←
+//Shortcut to move to the end of the line in Visual Studio Code: Ctrl + →
+
+//Shortcut to move a line up in Visual Studio Code: Alt + ↑
+//Shortcut to move a line down in Visual Studio Code: Alt + ↓
+
+//Shortcut to select a word in Visual Studio Code: Ctrl + D
+//Shortcut to select all occurrences of a word in Visual Studio Code: Ctrl + Shift + L
+
+//Shortcut to search for a word in Visual Studio Code: Ctrl + F
+//Shortcut to search and replace a word in Visual Studio Code: Ctrl + H
+
+//Shortcut to hide the sidebar in Visual Studio Code: Ctrl + B
+
+//Shortcut to open the terminal in Visual Studio Code: Ctrl + `
