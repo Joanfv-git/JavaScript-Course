2f89370121538266c5cee6fe2bc2a76959f231dd
JoanFV <joanfernandezvilana@gmail.com>
2024-07-31 20:02:45 +0200

    Declaración de variables

--- javascript-sandbox/01-variables-data-types/03-variables/script.js
+++ javascript-sandbox/01-variables-data-types/03-variables/script.js
@@ -1,12 +1,28 @@
 // Ways to declare a variable
 // var, let, const
 
+// var is the old way of declaring a variable
+var name = "John Doe";
+console.log(name);
+//let is the new way of declaring a variable
+let age = 30;
+console.log(age);
+//const is used to declare a constant variable
+const pi = 3.14;
+console.log(pi);
+
 // Naming Conventions
 // - Only letters, numbers, underscores and dollar signs
 // - Can't start with a number
+let _name = "John Doe";
+let $name = "John Doe";
+let name1 = "John Doe";
 
 // Multi-Word Formatting
 // firstName     camelCase
+let firstName = "John";
 // first_name    underscore
+let first_name = "John";
 // FirstName     PascalCase
+let FirstName = "John";
 // firstname     owercase
